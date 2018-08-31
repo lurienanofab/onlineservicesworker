865d1cacbb21226dbb540b8c404d6668eb796e60
Jim Getty <jgett@umich.edu>
2018-08-31 11:23:12 -0400

    Adding existing project files.

---
+++ OnlineServicesWorker/Service1.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.ServiceProcess;
+using System.Configuration.Install;
+using System.Linq;
+using System.Reflection;
+
+namespace OnlineServicesWorker
+{
+    public partial class Service1 : ServiceBase
+    {
+        public static readonly string InstallServiceName = "OnlineServicesWorker";
+
+        public Service1()
+        {
+            InitializeComponent();
+        }
+
+        public void Start(string[] args)
+        {
+            OnStart(args);
+            Program.ConsoleWriteLine("Press any key to exit.");
+        }
+
+        protected override void OnStart(string[] args)
+        {
+        }
+
+        protected override void OnStop()
+        {
+        }
+
+        public static bool IsServiceInstalled()
+        {
+            return ServiceController.GetServices().Any(s => s.ServiceName == InstallServiceName);
+        }
+
+        public static void InstallService()
+        {
+            if (IsServiceInstalled())
+            {
+                UninstallService();
+            }
+
+            ManagedInstallerClass.InstallHelper(new string[] { Assembly.GetExecutingAssembly().Location });
+        }
+
+        public static void UninstallService()
+        {
+            ManagedInstallerClass.InstallHelper(new string[] { "/u", Assembly.GetExecutingAssembly().Location });
+        }
+    }
+}
